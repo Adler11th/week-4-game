b847e6779d3394b5be65b1add7cfa1e7f7b384d1
Adler11th <mgoftest@gmail.com>
2018-02-12 14:01:26 -0800

    v1.2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,113 +1,133 @@
-//Class Declaration
-//=================
+$(document).ready(function () {
+    //Class Declaration
+    //=================
 
-class Hero {
-    constructor(Name, url, HP, Attk, CAttk) {
-        this.name = Name;
-        this.url = url;
-        this.health = HP;
-        this.attack = Attk;
-        this.counterAttack = CAttk;
-        this.selected = false;
-        this.available = true;
-    }
-}
-
-//Global Variables
-//================
-
-var GAMEWON = false;
-var PLAYERHERO = new Hero;
-var ENEMYHERO = new Hero;
-
-//Function Declaration
-//====================
-
-function displayHeroPull(objectHolder) {
-    for (var key in objectHolder) {
-        var elementID = objectHolder[key].name;
-        $(".hero_pull").append("<div class = hero-card id = " + elementID + "><img src =" + objectHolder[key].url + "></div>");
-        var tempID = "#" + elementID;
-        $(tempID).append("<h3 class = 'display_HP'>" + objectHolder[key].health + " HP</h3>");
-    }
-}
-
-function displayEnemyHero(object) {
-    var elementClass = ".hero_pull" + " #" + object.name;
-    $(elementClass).remove();
-    $(".enemy_hero div").remove();
-    $(".enemy_hero").append("<div class = hero-card id = " + object.name + "><img src =" + object.url + "></div>");
-    var tempID = "#" + object.name;
-    $(tempID).append("<h3 class = 'display_HP'>" + object.health + " HP</h3>");
-}
-
-function displayPlayerHero(object) {
-    var elementClass = ".hero_pull" + " #" + object.name;
-    $(elementClass).remove();
-    var tempID = "#" + object.name;
-    $(tempID).remove();
-    $(".player_hero").append("<div class = hero-card id = " + object.name + "><img src =" + object.url + "></div>");
-    $(tempID).append("<h3 class = 'display_HP'>" + object.health + " HP</h3>");
-}
-
-function playerSelectHero(object, name) {
-    object[name].selected = true;
-    object[name].available = false;
-    return object[name];
-}
-function enemySelectHero(objectHolder) {
-    for (var key in objectHolder) {
-        if (objectHolder[key].available === true) {
-            objectHolder[key].available = false;
-            return objectHolder[key];
+    class Hero {
+        constructor(Name, url, HP, Attk, CAttk) {
+            this.name = Name;
+            this.url = url;
+            this.health = HP;
+            this.attack = Attk;
+            this.counterAttack = CAttk;
+            this.available = true;
         }
     }
-    GAMEWON = true;
-    return objectHolder[key];
-}
-//Main
-//====
 
-$(document).ready(function () {
-
-    $(".btn").hide();
-    
+    //Global Variables
+    //================
     var Heroes = {
         "Vampire": new Hero("Vampire", "assets/img/vampire.png", 100, 8, 8),
-        "Archer": new Hero("Archer", "assets/img/archer.png", 90, 10, 10),
+        "Archer": new Hero("Archer", "assets/img/archer.png", 70, 2, 10),
         "Defender": new Hero("Defender", "assets/img/defender.png", 130, 5, 5),
         "Mage": new Hero("Mage", "assets/img/mage.png", 80, 8, 10)
     };
 
-    displayHeroPull(Heroes);
+    var PLAYERHERO = new Hero;
+    var ENEMYHERO = new Hero;
 
+    GAMEWON = false;
 
-    //player pick a character once
-    $(".hero-card").on("click", function () {
+    //Function Declaration
+    //====================
 
-        PLAYERHERO = playerSelectHero(Heroes, this.id);
+    function displayHeroPull(objectHolder) {
+        for (var key in objectHolder) {
+            var elementID = objectHolder[key].name;
+            $(".hero_pull").append("<div class = hero-card id = " + elementID + "><img src =" + objectHolder[key].url + "></div>");
+            var tempID = "#" + elementID;
+            $(tempID).append("<h3 class = 'display_HP'>" + objectHolder[key].health + " HP</h3>");
+        }
+    }
+
+    function displayEnemyHero(object) {
+        var elementClass = ".hero_pull" + " #" + object.name;
+        var tempID = "#" + object.name;
+        $(elementClass).remove();
+        $(".enemy_hero h2").remove();
+        $(".enemy_hero div").remove();
+        $(".enemy_hero").prepend("<h2>Enemy: " + object.name + "</h2><div class = hero-card id = " + object.name + "><img src =" + object.url + "></div>");
+        $(tempID).append("<h3 class = 'display_HP'>" + object.health + " HP</h3>");
+    }
+
+    function displayPlayerHero(object) {
+        var elementClass = ".hero_pull" + " #" + object.name;
+        var tempID = "#" + object.name;
+        $(elementClass).remove();
+        $(tempID).remove();
+        $(".player_hero h2").remove();
+        $(".player_hero div").remove();
+        $(".player_hero").prepend("<h2>Hero: " + object.name + "</h2><div class = hero-card id = " + object.name + "><img src =" + object.url + "></div>");
+        $(tempID).append("<h3 class = 'display_HP'>" + object.health + " HP</h3>");
+    }
+
+    function playerSelectHero(objectHolder, key) {
+        objectHolder[key].available = false;
+        return objectHolder[key];
+    }
+
+    function enemySelectHero(objectHolder) {
+        for (var key in objectHolder) {
+            if (objectHolder[key].available === true) {
+                objectHolder[key].available = false;
+                return objectHolder[key];
+            }
+        }
+        GAMEWON = true;
+        return null;
+    }
+
+    function startGame(Heroes, id) {
+
+        $("#main").show();
+        $(".btn").show();
+
+        Object.assign(PLAYERHERO, playerSelectHero(Heroes, id));
 
         displayPlayerHero(PLAYERHERO);
 
-        ENEMYHERO = enemySelectHero(Heroes);
+        Object.assign(ENEMYHERO, enemySelectHero(Heroes));
 
         displayEnemyHero(ENEMYHERO);
 
         $(".hero-card").off("click");
-        $(this).css("border", "1px solid transparent");
+        $(".hero-card").css("border", "1px solid transparent");
         $(".hero-card").off("mouseenter mouseleave");
-        $(".btn").show();
-    })
 
-    $(".hero-card").hover(
-        function () {
+    }
+
+    function reset() {
+        GAMEWON = false;
+
+        $(".btn").hide();
+
+        $("#main").hide();
+
+        for (var key in Heroes) {
+            Heroes[key].available = true;
+        }
+
+        $(".hero_pull div").remove();
+
+        displayHeroPull(Heroes);
+
+        $(".hero-card").on("click", function () {
+            startGame(Heroes, this.id);
+        });
+
+        $(".hero-card").hover(function () {
             $(this).css("border", "1px solid #fd2856");
         }, function () {
             $(this).css("border", "1px solid transparent");
-        })
+        });
+    }
+
+    //Main
+    //====
+
+    reset();
 
     $("#attack").on("click", function () {
-        if (!GAMEWON) {
+        try {
             if (ENEMYHERO.health > PLAYERHERO.attack) {
                 if (PLAYERHERO.health > ENEMYHERO.counterAttack) {
                     ENEMYHERO.health -= PLAYERHERO.attack;
@@ -117,17 +137,26 @@ $(document).ready(function () {
                     displayEnemyHero(ENEMYHERO);
                 } else {
                     alert("You lost!");
+                    reset();
                 }
-            }else{
-                ENEMYHERO = enemySelectHero(Heroes)
+            } else {
+                Object.assign(ENEMYHERO, enemySelectHero(Heroes));
                 displayEnemyHero(ENEMYHERO);
+
+                if (GAMEWON) {
+                    alert("You Won!");
+                    reset();
+                }
             }
-        }else{
-            alert("You Won!");
+        }
+        catch (error) {
+            alert("Ooops, something went wrong")
         }
     })
-    $("#reset").on("click", function(){
-        window.location.reload();
+
+
+    $("#reset").on("click", function () {
+        reset();
     })
 
 })
