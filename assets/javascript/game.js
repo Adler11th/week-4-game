559ba96c2357fb0ad548afb2f7ccb3ad66988533
Adler11th <mgoftest@gmail.com>
2018-02-12 19:26:28 -0800

    v1.03

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     //================
     var Heroes = {
         "Vampire": new Hero("Vampire", "assets/img/vampire.png", 100, 8, 8),
-        "Archer": new Hero("Archer", "assets/img/archer.png", 70, 2, 10),
+        "Archer": new Hero("Archer", "assets/img/archer.png", 90, 2, 10),
         "Defender": new Hero("Defender", "assets/img/defender.png", 130, 5, 5),
         "Mage": new Hero("Mage", "assets/img/mage.png", 80, 8, 10)
     };
@@ -26,77 +26,115 @@ $(document).ready(function () {
     var ENEMYHERO = new Hero;
 
     GAMEWON = false;
+    GAMESTARTED = false;
 
     //Function Declaration
     //====================
 
     function displayHeroPull(objectHolder) {
         for (var key in objectHolder) {
-            var elementID = objectHolder[key].name;
-            $(".hero_pull").append("<div class = hero-card id = " + elementID + "><img src =" + objectHolder[key].url + "></div>");
-            var tempID = "#" + elementID;
-            $(tempID).append("<h3 class = 'display_HP'>" + objectHolder[key].health + " HP</h3>");
+            $(".hero_pull").append("<div class = hero-card id = " + objectHolder[key].name + "><img src =" + objectHolder[key].url + "></div>");
+            var selectorNewCardId = "#" + objectHolder[key].name;
+            $(selectorNewCardId).append("<h3 class = 'display_HP'>" + objectHolder[key].health + " HP</h3>");
         }
     }
 
-    function displayEnemyHero(object) {
-        var elementClass = ".hero_pull" + " #" + object.name;
-        var tempID = "#" + object.name;
-        $(elementClass).remove();
-        $(".enemy_hero h2").remove();
-        $(".enemy_hero div").remove();
-        $(".enemy_hero").prepend("<h2>Enemy: " + object.name + "</h2><div class = hero-card id = " + object.name + "><img src =" + object.url + "></div>");
-        $(tempID).append("<h3 class = 'display_HP'>" + object.health + " HP</h3>");
+    function displayHero(object, nodeClass) {
+
+        var selectorHeroPullCard = ".hero_pull "+ "#"+object.name;
+        var selectorOldCard = nodeClass + " div";
+        var selectorNewCardId = "#" + object.name;
+
+        $(selectorHeroPullCard).remove();
+        $(selectorOldCard).remove();
+        $(nodeClass).prepend("<div class= chosen_card id = " + object.name + "><img src =" + object.url + "></div>");
+        $(selectorNewCardId).append("<h3 class = 'display_HP'>" + object.health + " HP</h3>");
     }
 
-    function displayPlayerHero(object) {
-        var elementClass = ".hero_pull" + " #" + object.name;
-        var tempID = "#" + object.name;
-        $(elementClass).remove();
-        $(tempID).remove();
-        $(".player_hero h2").remove();
-        $(".player_hero div").remove();
-        $(".player_hero").prepend("<h2>Hero: " + object.name + "</h2><div class = hero-card id = " + object.name + "><img src =" + object.url + "></div>");
-        $(tempID).append("<h3 class = 'display_HP'>" + object.health + " HP</h3>");
-    }
-
-    function playerSelectHero(objectHolder, key) {
+    function selectHero(objectHolder, key) {
+        console.log(objectHolder[key]);
         objectHolder[key].available = false;
         return objectHolder[key];
     }
 
-    function enemySelectHero(objectHolder) {
-        for (var key in objectHolder) {
-            if (objectHolder[key].available === true) {
-                objectHolder[key].available = false;
-                return objectHolder[key];
+    function checkIfAvailable(){
+        for(var key in Heroes){
+            if(Heroes[key].available === true){
+                console.log("Heroes available");
+                return true;
             }
         }
-        GAMEWON = true;
-        return null;
+        console.log("Heroes not available");
+        return false;
     }
 
-    function startGame(Heroes, id) {
+    function startRound(Heroes) {
+        $(".enemy_hero div").remove();
+        $(".hero-card").hover(function () {
+            $(this).css("border", "1px solid #fd2856");
+        }, function () {
+            $(this).css("border", "1px solid transparent");
+        });
 
-        $("#main").show();
-        $(".btn").show();
+        $(".hero-card").on("click.round", function () {
+            $("#main").show();
+            $(".btn").show();
+            if (!GAMESTARTED) {
+                Object.assign(PLAYERHERO, selectHero(Heroes, this.id));
+                displayHero(PLAYERHERO, ".player_hero");
+                GAMESTARTED = true;
+                $("#dialog").text("Pick enemy!");
+            } else {
+                Object.assign(ENEMYHERO, selectHero(Heroes, this.id));
+                displayHero(ENEMYHERO, ".enemy_hero");
+                $(".hero-card").off("click,round");
+                $(".hero-card").css("border", "1px solid transparent");
+                $(".hero-card").off("mouseenter mouseleave");
+                $("#dialog").text("Attack!");
+                fight();
+            }
+        })
+    }
 
-        Object.assign(PLAYERHERO, playerSelectHero(Heroes, id));
+    function fight(){
 
-        displayPlayerHero(PLAYERHERO);
-
-        Object.assign(ENEMYHERO, enemySelectHero(Heroes));
-
-        displayEnemyHero(ENEMYHERO);
-
-        $(".hero-card").off("click");
-        $(".hero-card").css("border", "1px solid transparent");
-        $(".hero-card").off("mouseenter mouseleave");
-
+        $("#attack").on("click", function () {
+            if (PLAYERHERO.health > ENEMYHERO.counterAttack) {
+                if (ENEMYHERO.health > PLAYERHERO.attack) {
+                    ENEMYHERO.health -= PLAYERHERO.attack;
+                    PLAYERHERO.health -= ENEMYHERO.counterAttack;
+                    PLAYERHERO.attack += Heroes[PLAYERHERO.name].attack;
+                    displayHero(PLAYERHERO, ".player_hero");
+                    displayHero(ENEMYHERO, ".enemy_hero");
+                }else{
+                    if(checkIfAvailable()){
+                        $("#dialog").text("Enemy defeated, pick another hero.");
+                        $("#attack").off("click");
+                        startRound();
+                    }else{
+                        $("#dialog").text("You won!");
+                        $("#attack").off("click");
+                        alert("You Won!");
+                        reset();
+                    }
+                }
+    
+            } else {
+                $("#attack").off("click");
+                alert("You lost!");
+                reset();
+            }
+    
+        })
     }
 
     function reset() {
+
         GAMEWON = false;
+
+        GAMESTARTED = false;
+
+        $("#dialog").text("Pick your hero!");
 
         $(".btn").hide();
 
@@ -110,50 +148,13 @@ $(document).ready(function () {
 
         displayHeroPull(Heroes);
 
-        $(".hero-card").on("click", function () {
-            startGame(Heroes, this.id);
-        });
 
-        $(".hero-card").hover(function () {
-            $(this).css("border", "1px solid #fd2856");
-        }, function () {
-            $(this).css("border", "1px solid transparent");
-        });
+        startRound(Heroes);
     }
-
     //Main
     //====
 
     reset();
-
-    $("#attack").on("click", function () {
-        try {
-            if (ENEMYHERO.health > PLAYERHERO.attack) {
-                if (PLAYERHERO.health > ENEMYHERO.counterAttack) {
-                    ENEMYHERO.health -= PLAYERHERO.attack;
-                    PLAYERHERO.health -= ENEMYHERO.counterAttack;
-                    PLAYERHERO.attack += 5;
-                    displayPlayerHero(PLAYERHERO);
-                    displayEnemyHero(ENEMYHERO);
-                } else {
-                    alert("You lost!");
-                    reset();
-                }
-            } else {
-                Object.assign(ENEMYHERO, enemySelectHero(Heroes));
-                displayEnemyHero(ENEMYHERO);
-
-                if (GAMEWON) {
-                    alert("You Won!");
-                    reset();
-                }
-            }
-        }
-        catch (error) {
-            alert("Ooops, something went wrong")
-        }
-    })
-
 
     $("#reset").on("click", function () {
         reset();
